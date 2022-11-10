0e6df76d7d194118d7c7a0975412371eb1c10cc7
paulasalesr <108489698+paulasalesr@users.noreply.github.com>
2022-11-10 20:25:52 -0300

    Delete JAVASCRIPT directory

--- JAVASCRIPT/index.js
+++
@@ -1,9 +0,0 @@
-function togglemenu() {
-
-    let NavList = document.querySelector(".NavList");
-    let MenuIcon = document.querySelector (".menu-icon");
-    
-    NavList.classList.toggle("open");
-    MenuIcon.classList.toggle("bx-x")
-
-  }
